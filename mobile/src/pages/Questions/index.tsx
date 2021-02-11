54da52108e17b2788b9e5c6c17159bfd4b9784fe
GabrielBrotas <gabrielbrotas22@gmail.com>
2021-02-11 20:39:41 -0300

    questions state

--- mobile/src/pages/Questions/index.tsx
+++ mobile/src/pages/Questions/index.tsx
@@ -1,4 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
+import { useNavigation } from '@react-navigation/native';
+
+import { useSelector, useDispatch } from 'react-redux';
+import { getQuestions } from '../../redux/actions/questionsActions';
+import { StateProps } from '../../redux/store';
 
 import {
   Container,
@@ -11,36 +16,69 @@ import Button from '../../components/Button';
 
 import ElonMusk from '../../assets/elonmusk.jpg';
 
-const Questions: React.FC = () => {
+interface IQuestionProps {
+  route: {
+    params: {
+      category: string;
+    }
+  };
+}
+
+const Questions: React.FC<IQuestionProps> = ({ route: { params: { category } } }) => {
+  const navigation = useNavigation();
+  const dispatch = useDispatch();
+
+  const [correctAnswers, setCorrectAnswers] = useState(0);
+  const [questionNumber, setQuestionNumber] = useState(0);
+
+  const { questions } = useSelector( (state: StateProps) => state.questions);
+
+  useEffect(() => {
+    dispatch(getQuestions());
+  }, [])
+
+  const handleAnswer = useCallback((isCorrectAnswer) => {
+    if (isCorrectAnswer) {
+      setCorrectAnswers(correctAnswers + 1);
+    };
+    setQuestionNumber(questionNumber + 1);
+
+    if (questions.length === questionNumber + 1) {
+      navigation.reset({
+        index: 0,
+        routes: [{name: 'EndGame', params: {correctAnswers}}],
+      });
+    }
+  }, [questionNumber, questions, correctAnswers]);
+
+  if (questions.length === 0) {
+    return <Text>Loading...</Text>
+  }
+
+  if (questionNumber === questions.length) {
+    return <Text>Loading..</Text>
+  }
+
   return (
     <Container style={{flex: 1, justifyContent: 'center'}}>
       <Image source={ElonMusk} />
 
-      <Text>
-        loremr eormseir mseoirm soeifms imsaoi dmasoid masodiamsdoimas doiamsd o?
-      </Text>
+        <Text>{questions[questionNumber].question
+          ? questions[questionNumber].question
+          : 'aa'
+          }</Text>
 
-      <ButtonsContainer>
-        <Button primary={false}>
-          Resposta numero 1
-        </Button>
-
-        <Button primary={false}>
-          Resposta numero 1
-        </Button>
-
-        <Button primary={false}>
-          Resposta numero 1
-        </Button>
-
-        <Button primary={false}>
-          Resposta numero 1
-        </Button>
-
-        <Button primary={false}>
-          Resposta numero 1
-        </Button>
-      </ButtonsContainer>
+        <ButtonsContainer>
+          {questions[questionNumber].answers.map( answer => (
+            <Button
+              key={answer._id}
+              primary={false}
+              onPress={() => handleAnswer(answer.isCorrectAnswer)}
+            >
+              {answer.answer}
+            </Button>
+          ))}
+          </ButtonsContainer>
     </Container>
   )
 };
